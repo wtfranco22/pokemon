117e46669fde9af0fbfdf967be244722a3763643
wtfranco22 <francofranco_96@hotmail.com>
2023-07-18 01:31:43 -0300

    [UPDATE] mejoras en el modal, finalizado

--- client/src/components/ModalPokemon/ModalPokemon.jsx
+++ client/src/components/ModalPokemon/ModalPokemon.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './ModalPokemon.module.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { setShowModal } from '../../redux/actions';
 
 const ModalPokemon = () => {
@@ -8,12 +8,19 @@ const ModalPokemon = () => {
     const dispatch = useDispatch();
     const handleClick = () => { dispatch(setShowModal(false)); };
     return (
-        <div className={styles.loading}>
-            <button type='button' onClick={handleClick}>close</button>
+        <div className={styles.container}>
             <img src={pokemon?.image} alt={pokemon?.name} className={styles.image} />
-            <h3>NAME: {pokemon?.name}</h3>
-            <h3>TYPE: {pokemon?.types[0].name.toUpperCase()} {pokemon?.types[1] && `, ${pokemon?.types[1].name.toUpperCase()}`}</h3>
-            <Link to={`Details/${pokemon.id}`} onClick={handleClick} >Ver detalles</Link>
+            <button type='button' onClick={handleClick} className={styles.btn_close}>close</button>
+            <div className={styles.details}>
+                <div className={styles.description}>
+                    <h3>Name:  {pokemon?.name}</h3>
+
+                </div>
+                <div className={styles.description}>
+                    <h3>Types: {pokemon?.types[0].name.toLowerCase()} {pokemon?.types[1] && `, ${pokemon?.types[1].name.toUpperCase()}`}</h3>
+                </div>
+                <NavLink to={`Details/${pokemon.id}`} onClick={handleClick} className={styles.link}>Ver detalles</NavLink>
+            </div>
         </div>
     );
 };
