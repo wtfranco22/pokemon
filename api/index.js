f0a981dc2fb19b31ac1af867fbfb3cef6a2ab580
wtfranco22 <francofranco_96@hotmail.com>
2023-07-21 13:20:38 -0300

    [ADD] pasando las variables de entorno

--- api/index.js
+++ api/index.js
@@ -20,13 +20,15 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const seederTypes = require('./seeders');
+require('dotenv').config();
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true })
   .then(() => seederTypes())
   .then(() => {
-    server.listen(3001, () => {
-      console.log('%s listening at 3001'); // eslint-disable-line no-console
+    server.listen(PORT, () => {
+      console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
     });
   })
   .catch((error) => console.log('ERROR UP SERVER--->' + error.message));
--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { DB, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB}`,
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB}`,
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
