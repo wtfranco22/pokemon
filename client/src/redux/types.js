e9c017e1e84e81e02771d9dbe4ba17cb791a9e60
wtfranco22 <francofranco_96@hotmail.com>
2023-07-21 13:29:11 -0300

    [ADD] agregando nueva ruta para solicitudes al back

--- client/src/redux/types.js
+++ client/src/redux/types.js
@@ -1,7 +1,7 @@
 export const LOADING_POKEMONS = 'LOADING_POKEMONS';
 export const SET_ACCESS = 'SET_ACCESS';
 export const GET_POKEMON_DETAIL = 'GET_POKEMON_DETAIL';
-export const URL = 'http://localhost:3001/pokemons';
+export const URL = 'https://pokemon-production-cc31.up.railway.app/';
 export const CLEAN_POKEMON_DETAIL = 'CLEAN_POKEMON_DETAIL';
 export const CREATE_POKEMON = 'CREATE_POKEMON';
 export const SET_INDEX_PAGE = 'SET_INDEX_PAGE';
